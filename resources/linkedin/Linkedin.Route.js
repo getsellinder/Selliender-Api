51ba044a8cbabce2761dc7bff14bba2f08678a8c
projectaudiostream <projectaudiostream@gmail.com>
2025-10-07 13:14:12 +0530

    remove-multper-in-linkedin

--- resources/linkedin/Linkedin.Route.js
+++ resources/linkedin/Linkedin.Route.js
@@ -6,11 +6,13 @@ const Router = express.Router()
 const upload = multer()
 
 
-Router.post("/create/file/:id", upload.fields([
-    { name: "content", maxCount: 1 }, // for LinkedinContent JSON
-    { name: "posts", maxCount: 1 }    // for LinkedinPost JSON
-]), LinkedinUploadFile)
+// Router.post("/create/file/:id", upload.fields([
+//     { name: "content", maxCount: 1 }, // for LinkedinContent JSON
+//     { name: "posts", maxCount: 1 }    // for LinkedinPost JSON
+// ]), LinkedinUploadFile)
 
+
+Router.post("/create/file/:id", LinkedinUploadFile)
 
 
 Router.get("/get/:id", getLinkedinUploadFile)
